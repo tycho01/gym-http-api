12b07441934fa36b0d91adf17d723c7814fc056c
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-05-31 23:44:13 +0200

    server: add timer helper

--- gym_http_server.py
+++ gym_http_server.py
@@ -9,10 +9,24 @@ import argparse
 import sys
 import json
 
+from timeit import default_timer as timer
+import cProfile
 
 import logging
 logger = logging.getLogger('werkzeug')
 logger.setLevel(logging.ERROR)
+
+
+class Timer(object):
+    def __init__(self, label="default"):
+        self.label = label
+
+    def __enter__(self):
+        self.start = timer()
+
+    def __exit__(self, type, value, tb):
+        end = timer()
+        print(self.label + ": " + str(end - self.start))
 
 
 class MyResponse(Response):
