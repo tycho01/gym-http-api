7ae3e81e46a354114f9f2d3c6ee3da884005af58
Tycho Grouwstra <tychogrouwstra@gmail.com>
2018-06-07 23:17:10 +0200

    I did random CartPole by myself! :)

--- binding-hs/src/Agents/Random.hs
+++ binding-hs/src/Agents/Random.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE UnicodeSyntax #-}
+
 -------------------------------------------------------------------------------
 -- |
 -- Module    :  Agents.Random
@@ -10,10 +11,23 @@
 -------------------------------------------------------------------------------
 module Agents.Random (RandomAgent (..)) where
 
-import           OpenAI.Gym (Action (..), ActionSpace, Agent (..), EnvSpec (..),
-                             Info (..), ObservationSpace, envActionSpaceSample)
+import           Control.Monad.IO.Class (liftIO)
+import Debug.Dump (d)
+import System.Random (randomRIO)
+import           Data.Aeson.Types       (Value (..))
+import           Data.Scientific        (scientific)
+import           OpenAI.Gym             (Action (..), ActionSpace (..),
+                                         Agent (..), EnvSpec (..), Info (..),
+                                         ObservationSpace (..), Observation (..), Space (..),
+                                         SpaceInfo (..), envActionSpaceSample)
 
 -- | an agent that acts randomly using the HTTP API's `envActionSpaceSample`: $a_{random} \in A$
 data RandomAgent spec actionSpace obsSpace = RandomAgent EnvSpec ActionSpace ObservationSpace
 instance Agent (RandomAgent spec actionSpace obsSpace) where
-  act (RandomAgent spec actionSpace obsSpace) ob t inst = envActionSpaceSample inst
+  act (RandomAgent spec actionSpace obsSpace) obs t inst = do
+    let aSpace = getSpaceInfo $ unActionSpace actionSpace
+    ac <- case aSpace of
+            Discrete n -> do
+              i <- liftIO $ randomRIO (0, n - 1)
+              return $ Number $ scientific (toInteger i) 0
+    return $ Action ac
